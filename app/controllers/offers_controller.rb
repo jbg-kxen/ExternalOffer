9966564d5ddd0659ad063ae392eeb1c24b94d9c9
samym <samy.mechiri@kxen.com>
2012-10-12 12:37:46 +0200

    force json by default

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -5,6 +5,7 @@ class OffersController < ApplicationController
   def getOffers
     @scoredOffers = HardOffer::OfferService.new.getOffers(params)
     respond_to do |format|
+      format.html  { render :json => @scoredOffers, :content_type => 'application/json' }
       format.json  { render :json => @scoredOffers }
     end
   end
